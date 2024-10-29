66f9ef8dd6970483748d4175ae02e2f6183a6643
mercede-s <89451211+mercede-s@users.noreply.github.com>
2024-10-29 16:39:29 +1300

    add and delete notes

--- app/src/main/java/com/example/notesapp/NoteAdapter.java
+++ app/src/main/java/com/example/notesapp/NoteAdapter.java
@@ -3,6 +3,7 @@ package com.example.notesapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,16 +12,17 @@ import java.util.ArrayList;
 public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
 
     private ArrayList<Note> notes;
+    private OnNoteClickListener onNoteClickListener;
 
-    public NoteAdapter(ArrayList<Note> notes) {
+    public NoteAdapter(ArrayList<Note> notes, OnNoteClickListener onNoteClickListener) {
         this.notes = notes;
+        this.onNoteClickListener = onNoteClickListener;
     }
 
     @NonNull
     @Override
     public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item, parent, false);
-        System.out.println("onCreateViewHolder called ");
         return new NoteViewHolder(view);
     }
 
@@ -28,7 +30,6 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder
     public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
         Note note = notes.get(position);
         holder.noteTextView.setText(note.getText());
-        System.out.println("onBindViewHolder called " + note.getText());
     }
 
     @Override
@@ -36,18 +37,50 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder
         return notes.size();
     }
 
-    public static class NoteViewHolder extends RecyclerView.ViewHolder {
+    public void addNote(Note newNote) {
+        notes.add(newNote);
+        notifyItemInserted(notes.size()-1);
+    }
+
+    public void updateNote(int index, Note newNote) {
+        notes.set(index, newNote);
+        notifyItemChanged(index);
+    }
+
+    public void removeNote(int position) {
+        notes.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public class NoteViewHolder extends RecyclerView.ViewHolder {
         TextView noteTextView;
 
         public NoteViewHolder(View itemView) {
             super(itemView);
             noteTextView = itemView.findViewById(R.id.noteTextView);
 
+            //Click listener for editing
+            itemView.setOnClickListener(v -> {
+                int position = getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
+
+                    onNoteClickListener.onNoteClick(position);
+                }
+            });
+
+            //LongClick listener for deleting
+            itemView.setOnLongClickListener(v -> {
+                int position = getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
+                    onNoteClickListener.onNoteLongClick(position);
+                }
+                return true;
+            });
         }
     }
 
-    public void addNote(Note note) {
-        notes.add(note);
-        notifyItemInserted(notes.size() - 1);
+    public interface OnNoteClickListener {
+        void onNoteClick(int position);
+        void onNoteLongClick(int position);
     }
 }
